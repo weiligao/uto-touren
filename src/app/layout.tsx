18f1e2e3508f5abd25f3c11fe6287f5252fc23a9
Weili Gao <9134665+weiligao@users.noreply.github.com>
2026-03-29 23:28:47 +0200

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "@/app/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -68,6 +69,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Footer />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
